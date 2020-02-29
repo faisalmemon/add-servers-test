0d54a4c44df41abacbecbf0c2dc881b541697d8b
Faisal Memon <faisalmemonuk@yahoo.com>
2020-02-29 11:44:20 +0000

    add basic infrastructure with vaion logo from vaion website

---
+++ vaion/vaion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vaion
+//
+//  Created by Faisal Memon on 29/02/2020.
+//  Copyright © 2020 Faisal Memon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
