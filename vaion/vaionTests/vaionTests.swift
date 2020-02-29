c4dd580861f776e97bd257774d43005385e47061
Faisal Memon <faisalmemonuk@yahoo.com>
2020-02-29 15:09:03 +0000

    rename test

--- vaion/vaionTests/vaionTests.swift
+++ vaion/vaionTests/vaionTests.swift
@@ -19,7 +19,7 @@ class vaionTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testCommenceAddServerNormalStartThenStop() {
+    func testCommenceAddServerThenStop() {
         var commenceCounter = 0
         
         let viewModel = WelcomeViewModel()
