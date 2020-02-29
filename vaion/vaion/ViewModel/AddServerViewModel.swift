b63da57f3948f3a214f9673835577fc63027ea36
Faisal Memon <faisalmemonuk@yahoo.com>
2020-02-29 17:06:20 +0000

    add add server view model logic

--- vaion/vaion/ViewModel/AddServerViewModel.swift
+++ vaion/vaion/ViewModel/AddServerViewModel.swift
@@ -10,17 +10,71 @@ import Foundation
 
 class AddServerViewModel {
     
-    var ipAddress: String?
+    var delayTimeoutSecond: TimeInterval = 5
+    var shouldShowSpinner = false
+    var ipAddress = ""
     
     var gotoServerCredentialsScreen: (() -> Void)?
     var gotoSuccessScreen: (() -> Void)?
+    var reportError: ((String) -> Void)?
+    var setSpinnerCallback:((Bool) -> Void)?
 
     
     func ipAddressString(updatedWith ipAddressString: String?) {
-        ipAddress = ipAddressString
+        ipAddress = ipAddressString ?? ""
     }
     
-    func nextButtonWasPressed() {
+    func showSpinner() {
+        if shouldShowSpinner {
+            
+            if let spinner = setSpinnerCallback {
+                spinner(true)
+            }
+        }
+    }
+    
+    func hideSpinner() {
+        if !shouldShowSpinner {
+            if let spinner = setSpinnerCallback {
+                spinner(false)
+            }
+        }
+    }
+    
+    func handleServerResponse(_ response: NetworkingResponse) {
+        self.shouldShowSpinner = false
+        self.hideSpinner()
+        
+        if response.success {
+            if let successScreen = self.gotoSuccessScreen {
+                successScreen()
+            }
+        } else if response.code == 401 {
+            if let credentialsScreen = self.gotoServerCredentialsScreen {
+                credentialsScreen()
+            }
+        } else {
+            if let doErrorReport = self.reportError {
+                /*
+                 Don't leak details of error to user in case of a malicous user was attempting
+                 to crack the login.
+                 */
+                doErrorReport(NSLocalizedString("Server error", comment: ""))
+            }
+        }
+    }
 
+    func nextButtonWasPressed() {
+        shouldShowSpinner = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + delayTimeoutSecond) {
+            self.showSpinner()
+        }
+        DispatchQueue.global().async {
+            Networking.sharedInstance.connectToServer(ipAddress: self.ipAddress, credentials: nil) { (response) in
+                DispatchQueue.main.async {
+                    self.handleServerResponse(response)
+                }
+            }
+        }
     }
 }
