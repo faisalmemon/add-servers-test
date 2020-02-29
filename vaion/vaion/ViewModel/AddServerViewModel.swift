01d0ba895888aba1e18225955d16b3655a2786fc
Faisal Memon <faisalmemonuk@yahoo.com>
2020-02-29 17:09:05 +0000

    tidy up view model

--- vaion/vaion/ViewModel/AddServerViewModel.swift
+++ vaion/vaion/ViewModel/AddServerViewModel.swift
@@ -10,23 +10,23 @@ import Foundation
 
 class AddServerViewModel {
     
+    //MARK: - State
+    
     var delayTimeoutSecond: TimeInterval = 5
     var shouldShowSpinner = false
     var ipAddress = ""
     
+    //MARK: - User Interface Callbacks
+
     var gotoServerCredentialsScreen: (() -> Void)?
     var gotoSuccessScreen: (() -> Void)?
     var reportError: ((String) -> Void)?
     var setSpinnerCallback:((Bool) -> Void)?
 
-    
-    func ipAddressString(updatedWith ipAddressString: String?) {
-        ipAddress = ipAddressString ?? ""
-    }
+    //MARK: - Internal Logic
     
     func showSpinner() {
         if shouldShowSpinner {
-            
             if let spinner = setSpinnerCallback {
                 spinner(true)
             }
@@ -63,7 +63,13 @@ class AddServerViewModel {
             }
         }
     }
+    
+    //MARK: - API
 
+    func ipAddressString(updatedWith ipAddressString: String?) {
+        ipAddress = ipAddressString ?? ""
+    }
+    
     func nextButtonWasPressed() {
         shouldShowSpinner = true
         DispatchQueue.main.asyncAfter(deadline: .now() + delayTimeoutSecond) {
