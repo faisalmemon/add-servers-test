a1496c963bd5256d5ebc4d18109de3e31e685e89
Faisal Memon <faisalmemonuk@yahoo.com>
2020-03-01 09:46:47 +0000

    move add server tests into their own file

--- vaion/vaionTests/addServerTests.swift
+++ vaion/vaionTests/addServerTests.swift
@@ -34,6 +34,80 @@ class AddServerMock: AddServerProtocol {
         }
         showingSpinner = show
     }
+}
+
+class vaionAddServerTests: XCTestCase {
     
+    func testSettingIpAddress() {
+        let mock = AddServerMock()
+        let viewModel = AddServerViewModel(callbackHandler: mock)
+        viewModel.ipAddress = "placeholder"
+        viewModel.ipAddressString(updatedWith: "1.2.3.4")
+        XCTAssert(viewModel.ipAddress == "1.2.3.4")
+    }
     
+    func testSettingAbsentIpAddressIsEmptyString() {
+        let mock = AddServerMock()
+        let viewModel = AddServerViewModel(callbackHandler: mock)
+        viewModel.ipAddress = "placeholder"
+        viewModel.ipAddressString(updatedWith: nil)
+        XCTAssert(viewModel.ipAddress == "")
+    }
+    
+    func testNextButtonWhenNoCredentialsRequired() {
+        let mock = AddServerMock()
+        let viewModel = AddServerViewModel(callbackHandler: mock)
+        
+        viewModel.ipAddressString(updatedWith: TestData.noCredentialsServer)
+        
+        viewModel.nextButtonWasPressed {
+            if mock.wentToSuccessScreen && !mock.wentToCredentialsScreen {
+                // good path
+            } else {
+                XCTFail("Did not go to the success screen")
+            }
+        }
+    }
+    
+    func testNextButtonWhenCredentialsAreRequired() {
+        let mock = AddServerMock()
+        let viewModel = AddServerViewModel(callbackHandler: mock)
+        
+        viewModel.ipAddressString(updatedWith: TestData.requireCredentialsServer)
+        
+        viewModel.nextButtonWasPressed {
+            if !mock.wentToSuccessScreen && mock.wentToCredentialsScreen {
+                // good path
+            } else {
+                XCTFail("Did not go to the credentials screen")
+            }
+        }
+    }
+    
+    func testNextButtonWhenServerUnknown() {
+        let mock = AddServerMock()
+        let viewModel = AddServerViewModel(callbackHandler: mock)
+        viewModel.ipAddressString(updatedWith: TestData.unknownServer)
+        
+        viewModel.nextButtonWasPressed {
+            if mock.wentToReportError && !mock.wentToSuccessScreen && !mock.wentToCredentialsScreen {
+                // good path
+            } else {
+                XCTFail("Did not go to the error screen")
+            }
+        }
+    }
+    
+    func testNextButtonSpinnerAppears() {
+        let mock = AddServerMock()
+        let viewModel = AddServerViewModel(callbackHandler: mock)
+                
+        viewModel.delayTimeoutSecond = TestData.networkServerShortestResponseTimeSeconds / 10.0
+        
+        viewModel.ipAddressString(updatedWith: TestData.noCredentialsServer)
+        
+        viewModel.nextButtonWasPressed() {
+            XCTAssert(mock.showedSpinnerAtLeastOnce)
+        }
+    }
 }
