4efc865db69c587d533f6ec7f07495346da9291e
Faisal Memon <faisalmemonuk@yahoo.com>
2020-03-01 16:59:59 +0000

    ensure ui works on the smallest screen

--- vaion/vaion/View/AddServerViewController.swift
+++ vaion/vaion/View/AddServerViewController.swift
@@ -40,16 +40,6 @@ class AddServerViewController: UIViewController {
         viewModel.nextButtonWasPressed()
     }
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let addCredentialsViewController = segue.destination as? AddCredentialsViewController {
             addCredentialsViewController.addServerViewModel = viewModel
